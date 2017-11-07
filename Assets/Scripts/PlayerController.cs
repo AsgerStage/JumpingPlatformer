73de5f7e0ae347a5f8f68b910fa2f79a3b18c095
Asger <asger.stage@hotmail.com>
2017-11-07 14:01:54 +0100

    Player movement updated
    Now uses bounciness to gain speed with an upper boundary on vertical speed that is increased by pressing space

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,76 +2,93 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
 
 
     public int playerSpeed = 40;
     public bool facingRight = false;
-    public int playerJumpPower=100;
+    //public int playerJumpPower=100; //outphased
     public float moveX;
-    private bool hasBeenGrounded;
-    public float maxSpeed = 20f;
-   
+    // private bool hasBeenGrounded; //outphased
+    public float maxSpeed = 4f;
+    private Rigidbody2D rigidbody;
+
+
     private void Start()
 
     {
-        Jump();
+        rigidbody = GetComponent<Rigidbody2D>();
+        rigidbody.collisionDetectionMode = CollisionDetectionMode2D.Continuous;
+        //  Jump();
     }
     // Update is called once per frame
-    void Update () {
-        playerMove();
-	}
+    void Update()
+    {
+        
+    }
     void playerMove()
     {
         //CONTROLS
         moveX = Input.GetAxis("Horizontal");
-        if (Input.GetButtonDown("Jump") && hasBeenGrounded==true)
+        if (Input.GetButton("Jump"))
         {
-            Jump();
+            maxSpeed = 6;
+            // Jump();
         }
-       
-        
-        
+        else maxSpeed = 4;
+
+
+
         //ANIMATIONS
         //PLAYERDIRECTION
-        if(moveX <0.0f && facingRight == false)
-        {
-            FlipPlayer();
-        }
-        else if (moveX>0.0f && facingRight == true)
-        {
-            FlipPlayer();
-        }
+        /*  if(moveX <0.0f && facingRight == false)
+          {
+              FlipPlayer();
+          }
+          else if (moveX>0.0f && facingRight == true)
+          {
+              FlipPlayer();
+          }*/
         //PHYSICS
-        gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(moveX * playerSpeed, gameObject.GetComponent<Rigidbody2D>().velocity.y);
+        rigidbody.velocity = new Vector2(moveX * playerSpeed, rigidbody.velocity.y);
+        
+       
     }
 
-    private void FixedUpdate()
+    private void FixedUpdate() 
     {
-        if (gameObject.GetComponent<Rigidbody2D>().velocity.y > maxSpeed)
+        playerMove();
+        if (rigidbody.velocity.y > maxSpeed)
         {
-            gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(gameObject.GetComponent<Rigidbody2D>().velocity.x, maxSpeed);
+            rigidbody.velocity = new Vector2(gameObject.GetComponent<Rigidbody2D>().velocity.x, maxSpeed);
         }
     }
-    void Jump()
+    /*void Jump()
     {
-        //JUMPING CODE
+        //outdated jumping code
+        //Unused
         GetComponent<Rigidbody2D>().AddForce(Vector2.up * playerJumpPower);
         hasBeenGrounded = false;
 
-    }
-    void FlipPlayer()
-    {
-        facingRight = !facingRight;
-        Vector2 localScale = gameObject.transform.localScale;
-        localScale.x *= -1;
-        transform.localScale = localScale;
-    }
+    }*/
+    /*  void FlipPlayer() //unused
+      {
+          facingRight = !facingRight;
+          Vector2 localScale = gameObject.transform.localScale;
+          localScale.x *= -1;
+          transform.localScale = localScale;
+      }*/
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.tag == "Ground")
+        /*  if (collision.gameObject.tag == "Ground")
+          {
+              hasBeenGrounded = true;
+          }*/
+
+        if (collision.gameObject.name == "Wall")  // or if(gameObject.CompareTag("YourWallTag"))
         {
-            hasBeenGrounded = true;
+            rigidbody.velocity = Vector2.zero;
         }
     }
 }
