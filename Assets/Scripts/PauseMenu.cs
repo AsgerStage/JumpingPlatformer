956bcb7dfd9b594740c767e328a17ee97a113cf8
IamHexor <ykhexor@hotmail.com>
2017-12-10 15:23:10 +0100

    new features
    added main and pause menu, added coins to collect and toxic objects that kills you

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class PauseMenu : MonoBehaviour {
+
+    GameObject pauseMenu;
+    bool paused;
+
+	// Use this for initialization
+	void Start () {
+        paused = false;
+        pauseMenu = GameObject.Find("PauseMenu");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            paused = !paused;
+            pauseMenu.SetActive(true);
+            Time.timeScale = 0;
+        }
+        else if (!paused)
+        {
+            pauseMenu.SetActive(false);
+            Time.timeScale = 1;
+        }
+		
+	}
+
+
+    public void resumeGame()
+    {
+        paused = false;
+    }
+
+    public void QuitToMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+}
